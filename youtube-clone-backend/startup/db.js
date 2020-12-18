fbf2fdac1ba44ebc1855abbab6ea83941c73e66f
brooke <daisyrakionvideos@gmail.com>
2020-12-17 18:30:40 -0600

    mongodb is now connected and working

--- youtube-clone-backend/startup/db.js
+++ youtube-clone-backend/startup/db.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-<<<<<<< HEAD
 const config = require('config');
 const { connect } = require('mongodb');
 
@@ -16,23 +15,3 @@ mongoose.connect(
 }
 
 module.exports = connectDB;
-=======
-const config = require('congif');
-
-
-function connectDB(){
-mongoose.connect(
-    config.get('mongoURI'), 
-    { useNewUrlParser: true, useUnifiedTopology: true })
-.then(()=> console.log('Connected to mongoDB...'))
-.catch((err) => {
-     console.log(`Could not connect to mongoDB. ERROR: ${err}`);
-     process.exit(1);
-    });
-
-}
-
-
-
-module.exports = connectDB;
->>>>>>> 09b0d44c1131e26456e721203e0df6bbc7535b6f
