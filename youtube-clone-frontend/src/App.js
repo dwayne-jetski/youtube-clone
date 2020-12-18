8c686293fd771919b6f994b52a1c96948849d5ca
marcus-baumgardner <dwayne.jerski@gmail.comgit config --global user.name marcus-baumgardnergit config --global user.email dwayne.jerski@gmail.com>
2020-12-17 20:50:33 -0600

    added several component files, made searchbar form input update to state

--- youtube-clone-frontend/src/App.js
+++ youtube-clone-frontend/src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import NavBar from './components/navBar/navBar'
+import HomePageContent from './components/homePageContent/homePageContent'
+import DisplaySearchResults from './components/searchResultsContent/searchResultsContent'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      viewingHomePage: true,
+      viewingSearchResults: false,
+      viewingVideoPlayer: false,
+      searchBarVal: '',
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    
+  }
+
+  handleChange(event){
+      
+    let nam = event.target.name;
+    let val = event.target.value;
+    this.setState({[nam]: val});
+
+  }
+
+  handleSubmit(event){
+
+    event.preventDefault();
+
+    this.setState({
+      viewingHomePage: false,
+      viewingSearchResults: true,
+      viewingVideoPlayer: true,
+    })
+    
+
+  }
+
+
+  
+
+
+  render(){
+
+    console.log('viewing home page?: ', this.state.viewingHomePage);
+    console.log('viewing Search results?: ', this.state.viewingSearchResults);
+    console.log('viewing video player?: ', this.state.viewingVideoPlayer);
+    console.log('Search Bar Value: ', this.state.searchBarVal)
+
+    if(this.state.viewingHomePage === true && this.state.viewingSearchResults === false && this.state.viewingVideoPlayer === false){
+      return(
+        <React.Fragment bg="dark">
+          <NavBar handleSearchbarChange={()=> this.handleChange} handleSearchSumbit={()=>this.handleSubmit}/>
+          <HomePageContent/>
+        </React.Fragment>
+      )
+    } else if(this.state.viewingHomePage === false && this.state.viewingSearchResults === true && this.state.viewingVideoPlayer === false){
+      return(
+        <React.Fragment bg="dark">
+          <NavBar handleSearchbarChange={()=> this.handleChange} handleSumbit={()=>this.handleSubmit}/>
+          <DisplaySearchResults/>
+        </React.Fragment>
+      )
+    } else if(this.state.viewingHomePage === false && this.state.viewingSearchResults === false && this.state.viewingVideoPlayer === true){
+      return(
+        <React.Fragment bg="dark">
+          <NavBar handleSearchbarChange={()=> this.handleChange} handleSumbit={()=>this.handleSubmit}/>
+          <HomePageContent/>
+        </React.Fragment>
+      )
+    }
+
+
+  }
+
 }
 
 export default App;
---
+++ youtube-clone-frontend/src/components/navBar/navBar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import FormControl from 'react-bootstrap/FormControl';
+import './navBar.css'
+
+
+function NavBar(props){
+
+
+    return(
+
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Brand className='col-md-4'>Definitely Not Youtube</Navbar.Brand>
+            <Form inline className='search-bar col-md-6' onSubmit={()=> props.handleSearchSubmit()}>
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" name='searchBarVal' onChange={props.handleSearchbarChange()} />
+                <Button type='submit' value='Submit'></Button> 
+            </Form>
+        </Navbar>
+    )
+}
+
+
+export default NavBar;
